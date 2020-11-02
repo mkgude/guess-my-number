788852a21d34c48b3e11a263520f241134061d1d
Marian Gude <mkgude@gmail.com>
2020-11-02 15:47:54 -0600

    completed game experience when selecting again to start over

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let score = 20
-const num = Math.trunc(Math.random()*20)+1;
+let num = Math.trunc(Math.random()*20)+1;
 
 document.querySelector(".check").addEventListener("click", function() {
     const guess = Number(document.querySelector(".guess").value)
@@ -32,5 +32,17 @@ document.querySelector(".check").addEventListener("click", function() {
             document.querySelector(".score").textContent = 0
         }
     }
+})
+
+document.querySelector(".again").addEventListener("click", function(){
+    score = 20;
+    num = Math.trunc(Math.random()*20)+1;
+    document.querySelector(".message").textContent = "Start guessing...";
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".number").textContent="?"
+    document.querySelector(".guess").value = ""
+    
+    document.querySelector("body").style.backgroundColor= "#222"
+    document.querySelector(".number").style.width = "15rem"
 })
 
